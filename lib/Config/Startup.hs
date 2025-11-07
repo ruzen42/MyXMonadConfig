a8238432577d8801db487dda55b4529856f4c10f
ruzen42- <malledeferlive@gmail.com>
2025-11-07 22:09:20 +0500

    fix

--- lib/Config/Startup.hs
+++ lib/Config/Startup.hs
@@ -2,22 +2,16 @@ module Config.Startup where
 
 import XMonad
 import XMonad.Util.SpawnOnce
-import Control.Monad (mapM_) 
+import Control.Monad (mapM_)
 import Data.Text
 
 myStartupCommands :: [String]
 myStartupCommands =
     [ "picom -b"
     , "feh --bg-scale /home/ruzen42/background.png"
+    , "xrandr --output DisplayPort-1 --rate 165"
+    , "alacritty"
     ]
 
 myStartupHook :: X ()
-<<<<<<< HEAD
 myStartupHook = mapM_ spawnOnce myStartupCommands
-=======
-myStartupHook = do
-	spawnOnce "picom -b"
-	spawnOnce "feh --bg-scale /home/ruzen42/background.png"
-	spawnOnce "taffybar"
-	spawnOnce "xrandr --output DP-2 --rate 165"
->>>>>>> 181918d (fix)
